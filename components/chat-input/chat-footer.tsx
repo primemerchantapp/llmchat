afbd9513975979d20b15bda1fe98204818ebc01f
masterdee121212 <primemerchantapp@gmail.com>
2024-10-10 02:36:00 +0800

    Update chat-footer.tsx

--- components/chat-input/chat-footer.tsx
+++ components/chat-input/chat-footer.tsx
@@ -1,5 +1,4 @@
 import { Flex, Type } from "@/ui";
-import Link from "next/link";
 
 export const ChatFooter = () => {
   return (
@@ -9,14 +8,7 @@ export const ChatFooter = () => {
         textColor="tertiary"
         className="inline-block text-center"
       >
-        LLMChat is open source and your data is stored locally. project by{" "}
-        <Link
-          href="https://trendy.design"
-          target="_blank"
-          className="inline-block text-violet-500 underline decoration-zinc-500/20 underline-offset-2"
-        >
-          trendy.design
-        </Link>
+        aiConnect by Emilio LLM
       </Type>
     </Flex>
   );
